bb3a26fc0f88b33e87c2fc488d99eaf06f21aebe
Guy Shani <guy.shani@post.uni.ac.il>
2024-11-06 15:48:17 +0200

    testetst

--- Bill3.java
+++ Bill3.java
@@ -2,7 +2,14 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
+		System.out.println("Please Enter Your Names: ");
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+	    String name2 = args[1];
+		String name3 = args[2]; 
+		System.out.println("Please Enter Total Price: ");
+		double price =  Double.parseDouble(args[3]);
+		
+		System.out.println(name1 + "," + name2 + "," + name3 + "you need to pay: " + Math.ceil(price / 3));
+
 	}
 }
