55ff75db187a0b1c997c9e20dc11ad949b5d751d
Guy Shani <guy.shani@post.uni.ac.il>
2024-11-07 16:02:55 +0200

    zzz

--- Bill3.java
+++ Bill3.java
@@ -10,6 +10,6 @@ public class Bill3 {
 		//Price
 		double price = Double.parseDouble(args[3]);
 
-		System.out.println("dear " + name1 + "," + name2 + "," + name3 + " you need to pay: " + Math.ceil(price / 3));
+		System.out.println("dear " + name1 + "," + name2 + "," + name3 + ": pay " + Math.ceil(price / 3) + "Shekels each");
 	}
 }
