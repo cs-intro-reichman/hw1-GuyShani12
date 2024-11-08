33eeb815ad09ac3110c10af4353dd4f95c5e8832
Guy Shani <guy.shani@post.uni.ac.il>
2024-11-08 13:28:50 +0200

    first chang of the program

--- TimeFormat.java
+++ TimeFormat.java
@@ -11,10 +11,10 @@ public class TimeFormat {
 			if (hours < 10)
 				System.out.print("0" + hours + ":" + minutes + " AM");
 				else if (hours >= 12)
-				System.out.print(hours + ":0" + minutes +" PM");
+				System.out.print((hours - 12) + ":0" + minutes +" PM");
 	    }
 		else if (hours >= 12)
-		System.out.print(hours + ":" + minutes + " PM");
+		System.out.print((hours - 12) + ":" + minutes + " PM");
 		else 
 		System.out.print(hours + ":" + minutes + " AM");
 	}
