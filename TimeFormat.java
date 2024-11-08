eff64a4f5b893fb8a4bbdd2ce2a266b8f671567f
Guy Shani <guy.shani@post.uni.ac.il>
2024-11-08 13:21:39 +0200

    first chang of the program

--- TimeFormat.java
+++ TimeFormat.java
@@ -1,11 +1,21 @@
 // Represents the hh:mm time format using an AM/PM format. 
 public class TimeFormat {
 	public static void main(String[] args) {
-		String time = args[0];
-		
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
-		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        // Replace this comment with the rest of your code
+
+		if (hours < 10 || minutes < 10)
+		{
+			if (hours < 10 && minutes < 10)
+				System.out.print("0" + hours + ":0" + minutes +" AM");
+			if (hours < 10)
+				System.out.print("0" + hours + ":" + minutes + " AM");
+				else if (hours >= 12)
+				System.out.print(hours + ":0" + minutes +" PM");
+	    }
+		else if (hours >= 12)
+		System.out.print(hours + ":" + minutes + " PM");
+		else 
+		System.out.print(hours + ":" + minutes + " AM");
 	}
 }
